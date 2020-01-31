6e2b32c2a128d6dccef622befb4976a116b3399b
Matthijs <D275511@edu.rocwb.nl>
2020-01-31 11:51:52 +0100

    init

---
+++ eerste les.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>  
+<html>  
+<head>  
+    <title>Vliegtuigen Toevoegen</title> 
+	<link rel="stylesheet" href="Eerste les.css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"/>
+</head>  
+<body>  
+<div class="wrapper">
+  <nav class="tabs">
+    <div class="selector"></div>
+    <a class="active" href="eerste les.php"><i class="fa fa-fw fa-home"></i>Home</a>
+    <a href="toevoegen.php"><i class="fas fa-hand-rock"></i>Toevoegen</a>
+    <a href="#"><i class="fas fa-bolt"></i>Thor</a>
+    <a href="#"><i class="fas fa-burn"></i>Marvel</a>
+  </nav>
+</div>
+<script>
+	var tabs = $('.tabs');
+	var selector = $('.tabs').find('a').length;
+	//var selector = $(".tabs").find(".selector");
+	var activeItem = tabs.find('.active');
+	var activeWidth = activeItem.innerWidth();
+	$(".selector").css({
+	  "left": activeItem.position.left + "px", 
+	  "width": activeWidth + "px"
+	});
+
+	$(".tabs").on("click","a",function(e){
+	  e.preventDefault();
+	  $('.tabs a').removeClass("active");
+	  $(this).addClass('active');
+	  var activeWidth = $(this).innerWidth();
+	  var itemPos = $(this).position();
+	  $(".selector").css({
+		"left":itemPos.left + "px", 
+		"width": activeWidth + "px"
+	  });
+	});
+</script>
+<h1>Voer hier vliegtuigen in</h1>  
+<p><em>Welkom</em></p>  
+</body>  
+</html>  
